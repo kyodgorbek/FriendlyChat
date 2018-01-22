70f1d43ade7ba40329de6fcc9049cba71c70ebe5
kyodgorbek <kyodgorbek@gmail.com>
2018-01-22 23:48:42 +0100

    I have updated code and implemented firebase signing

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -17,6 +17,7 @@ package com.google.firebase.udacity.friendlychat;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -35,6 +36,8 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -42,6 +45,9 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -65,7 +71,11 @@ public class MainActivity extends AppCompatActivity {
     private ChildEventListener mChildEventListener;
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseStorage mFirebaseStorage;
+    private StorageReference mChatPhotosStorageReference;
     public static final int RC_SIGN_IN = 1;
+    private static final int RC_PHOTO_PICKER = 2;
+    
 
     private String mUsername;
 
@@ -77,7 +87,10 @@ public class MainActivity extends AppCompatActivity {
         mUsername = ANONYMOUS;
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseStorage = FirebaseStorage.getInstance();
         mMessageDatabaseReference = mFirebaseDatabase.getReference().child("messages");
+        mChatPhotosStorageReference = mFirebaseStorage.getReference().child("chat_photos");
+        
 
         // Initialize references to views
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
@@ -99,6 +112,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // TODO: Fire an intent to show an image picker
+             Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+              intent.setType("image/jpeg");
+              intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+              startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
+
             }
         });
 
@@ -171,6 +189,19 @@ public class MainActivity extends AppCompatActivity {
          } else if (resultCode == RESULT_CANCELED) {
              Toast.makeText(this, "Signed cancelled", Toast.LENGTH_SHORT).show();
              finish();
+         } else if(requestCode == RC_PHOTO_PICKER &&  resultCode == RESULT_OK){
+             Uri selectedImageUri = data.getData();
+             StorageReference photoRef =
+                     mChatPhotosStorageReference.child(selectedImageUri.getLastPathSegment());
+             // Upload File to Firebase Storage
+             photoRef.putFile(selectedImageUri).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                 @Override
+                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                 Uri downloadUrl =  taskSnapshot.getDownloadUrl();
+                 FriendlyMessage friendlyMessage = new FriendlyMessage(null, mUsername, downloadUrl.toString());
+                 mMessageDatabaseReference.push().setValue(friendlyMessage);
+                 }
+             });
          }
      }
 
@@ -205,9 +236,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
-    }
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu:
+                AuthUI.getInstance().signOut(this);
+                return true;
 
+                default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     private void onSignedInInitialize(String userName) {
         mUsername = userName;
