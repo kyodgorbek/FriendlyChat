7c290f2fd568c37aa95ad49c186b76cee628d457
kyodgorbek <kyodgorbek@gmail.com>
2018-01-22 18:09:42 +0100

    I have updated code and implemented firebase signing

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -16,6 +16,7 @@
 package com.google.firebase.udacity.friendlychat;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -160,6 +161,19 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+    }
+     @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+     super.onActivityResult(requestCode, resultCode, data);
+     if(requestCode == RC_SIGN_IN) {
+         if (resultCode == RESULT_OK) {
+             Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+         } else if (resultCode == RESULT_CANCELED) {
+             Toast.makeText(this, "Signed cancelled", Toast.LENGTH_SHORT).show();
+             finish();
+         }
+     }
+
     }
 
     @Override
